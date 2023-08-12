9c62e84dd58b6aad53f1822e526826b7737e3191
uleuw <uleuw0927@gmail.com>
2023-08-12 12:23:56 +0900

    .

--- 111.py
+++
@@ -1,9 +0,0 @@
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-print(list1)
-print(list2)
-list1.append(list2)
-list2.extend(list1)
-print(list1)
-print(list2)
-print(list1+list2)
--- 1차원배열/10871.py
+++ 1차원배열/10871.py
@@ -1,5 +1,6 @@
 N,X = map( int, (input().split()))
 A=list(map(int, input().split()))
-X=list
-a=(A.count(A<X))
-print(a)
+if A[False]<X:
+    print(A[False])
+if A[True]<X:
+    print(A[True])
